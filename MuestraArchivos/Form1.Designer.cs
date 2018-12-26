9f098c9e8c9ba193682b54fcdd1c93d08a7a4c0f
Alejandro Villaseca <alejandrovs88@gmail.com>
2018-12-26 12:39:23 -0300

    Primer commit

---
+++ MuestraArchivos/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MuestraArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxPath = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.rtxtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxPath
+            // 
+            this.txtBoxPath.Location = new System.Drawing.Point(69, 105);
+            this.txtBoxPath.Name = "txtBoxPath";
+            this.txtBoxPath.Size = new System.Drawing.Size(231, 20);
+            this.txtBoxPath.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(306, 103);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtxtOutput
+            // 
+            this.rtxtOutput.Location = new System.Drawing.Point(69, 188);
+            this.rtxtOutput.Name = "rtxtOutput";
+            this.rtxtOutput.Size = new System.Drawing.Size(642, 208);
+            this.rtxtOutput.TabIndex = 2;
+            this.rtxtOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtxtOutput);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtBoxPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxPath;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.RichTextBox rtxtOutput;
+    }
+}
+
---
+++ MuestraArchivos/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuestraArchivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"\\D406186E927BE\Cockpit$\Cockpit 2.0\Publicaciones\Release 2.1\Fuentes");//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.*", SearchOption.AllDirectories); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                try
+                {
+                    var _version = FileVersionInfo.GetVersionInfo(file.FullName).ProductVersion == null ? "" : FileVersionInfo.GetVersionInfo(file.FullName).ProductVersion;
+                    rtxtOutput.AppendText(file.DirectoryName + "\t" + file.Name + "\t" + file.LastWriteTime + "\t" + _version + System.Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+                
+            }
+            
+        }
+    }
+}
